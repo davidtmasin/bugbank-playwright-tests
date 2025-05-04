78e5823e38e0225ef8de521967daef0418d86e86
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-05-04 19:50:49 -0300

    test: criação dos testes na transferência de valores

--- pages/TransferPage.ts
+++ pages/TransferPage.ts
@@ -2,23 +2,55 @@ import { Locator, Page } from "@playwright/test";
 
 export class TransferPage {
     readonly page: Page;
+    readonly transferPageButton: Locator;
     readonly accountNumber: Locator;
     readonly accountDigit: Locator;
     readonly transferValue: Locator;
     readonly transferDescription: Locator;
     readonly transferButton: Locator;
+    readonly modalText: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.transferPageButton = page.locator('#btn-TRANSFERÊNCIA');
         this.accountNumber = page.locator('input[type="accountNumber"]');
         this.accountDigit = page.locator('input[type="digit"]');
         this.transferValue = page.locator('input[type="transferValue"]');
         this.transferDescription = page.locator('input[type="description"]');
-        this.transferButton = page.locator('button[type="submit"]');
+        this.transferButton = page.locator('text=Transferir agora');
+        this.modalText = page.locator('#modalText');
     }
 
     async goto() {
         await this.page.goto('/');
+    }
+
+    // async extractAccountNumber1() {
+    //     const message = await this.page.textContent('#modalText');
+    //     console.log(message);
+
+    //     const match = message?.match(/\d+/g); // pega todos os números (como strings)
+    //     const accountNumber1 = match ? parseInt(match[0]) : null;
+    //     const accountDigit1 = match ? parseInt(match[1]) : null;
+
+    //     console.log(accountNumber1);
+    //     console.log(accountDigit1);
+    // }
+
+    // async captureAccountDataWithBalance() {
+    //     const message = await this.page.textContent('#modalText');
+    //     console.log(message);
+
+    //     const match = message?.match(/\d+/g); // pega todos os números (como strings)
+    //     const accountNumber2 = match ? parseInt(match[0]) : null;
+    //     const accountDigit2 = match ? parseInt(match[1]) : null;
+
+    //     console.log(accountNumber2);
+    //     console.log(accountDigit2);
+    // }
+
+    async gotToTransferPage() {
+        await this.transferPageButton.click();
     }
 
     async fillTransferForm(
@@ -35,9 +67,9 @@ export class TransferPage {
         }
     ) {
         await this.accountNumber.fill(accountNumber);
-        await this.accountNumber.fill(accountDigit);
-        await this.accountNumber.fill(transferValue);
-        await this.accountNumber.fill(transferDescription);
+        await this.accountDigit.fill(accountDigit);
+        await this.transferValue.fill(transferValue);
+        await this.transferDescription.fill(transferDescription);
     }
 
     async submitTransfer() {
--- tests/transfer.spec.ts
+++ tests/transfer.spec.ts
@@ -1,10 +1,48 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '../fixtures/accounts';
+import { LoginPage } from "../pages/LoginPage";
 import { TransferPage } from "../pages/TransferPage";
 
+
 test.describe("C03 - Transferência de Valores", () => {
-    test('CT01 - Usar uma conta com saldo suficiente para transferir R$100 para uma conta válida', async ({ page }) => {
+
+    test.beforeEach(async ({ page }, testInfo) => {
+        await page.screenshot({ path: `screenshots/transferencia/C03-${testInfo.title}-ANTES.png`, fullPage: true });
+    });
+
+    test.afterEach(async ({ page }, testInfo) => {
+        await page.screenshot({ path: `screenshots/transferencia/C03-${testInfo.title}-DEPOIS.png`, fullPage: true });
+    });
+
+    test('CT01 - Usar uma conta com saldo suficiente para transferir R$100 para uma conta válida', async ({ page, originAccount, destinyAccount }) => {
+        const data = {
+            email: 'conta-com-saldo@teste.com',
+            password: 'S3cr3TP@55w0rd'
+        };
+        const login = new LoginPage(page);
         const transfer = new TransferPage(page);
 
+        // Login na conta com saldo
+        await login.credentials({
+            email: data.email,
+            password: data.password
+        });
+        await login.access();
+
+        // Ir para transferência
+        await transfer.gotToTransferPage();
+        await transfer.fillTransferForm({
+            accountNumber: destinyAccount.number,
+            accountDigit: destinyAccount.digit,
+            transferValue: '100',
+            transferDescription: 'transferência de teste'
+        });
+        await transfer.submitTransfer();
+
+        const msg = await page.textContent('#modalText');
+        expect(msg).toContain('Transferencia realizada com sucesso');
+
+        // await page.locator('text=Fechar').click();
+        // await expect(page).toHaveURL('/bank-statement')
     });
 
 });
