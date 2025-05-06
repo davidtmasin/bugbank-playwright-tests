90aa3058ff37ddc669ed326807415ef85028fb6d
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-05-06 13:31:51 -0300

    feat: adiciona arquivo dataTest.ts para centralizar dados sensíveis via variáveis de ambiente

---
+++ fixtures/dataTest.ts
@@ -0,0 +1,6 @@
+export const dataTestLogin = {
+  email: process.env.TEST_USER_EMAIL || '',
+  name: process.env.TEST_USER_NAME || '',
+  password: process.env.TEST_USER_PASSWORD || '',
+  passwordConfirmation: process.env.TEST_USER_PASSWORD || '',
+};
