5745bf878c67f5d4cec96bf19e51ab3b1fe483e9
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-05-06 16:58:39 -0300

    chore: config workers e screenshot

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,16 +23,19 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 'auto' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://bugbank.netlify.app',
 
     // Grava vídeo de todos os testes
     video: 'on', // outras opções: 'off', 'retain-on-failure', 'on-first-retry'
+    
+    screenshot: 'on',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
